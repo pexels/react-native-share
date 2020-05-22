395b771ad5253c6613fa8cf493086045800fdc44
Simon Gough <scgough@users.noreply.github.com>
2020-05-22 15:02:02 +0100

    stories - video share const

--- ios/RNShare.m
+++ ios/RNShare.m
@@ -103,6 +103,7 @@ RCT_EXPORT_MODULE()
     @"SHARE_BACKGROUND_IMAGE": @"shareBackgroundImage",
     @"SHARE_STICKER_IMAGE": @"shareStickerImage",
     @"SHARE_BACKGROUND_AND_STICKER_IMAGE": @"shareBackgroundAndStickerImage",
+    @"SHARE_BACKGROUND_VIDEO_AND_STICKER_IMAGE": @"shareBackgroundVideoAndStickerImage",
   };
 }
 
